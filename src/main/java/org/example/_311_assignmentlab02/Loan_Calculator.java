27188fd8f29b29b63b09476bc10e78707b91e845
maradC <maradiagachristopher5@gmail.com>
2024-09-13 21:13:24 -0400

    Had to recreate the GUI

---
+++ src/main/java/org/example/_311_assignmentlab02/Loan_Calculator.java
@@ -0,0 +1,22 @@
+package org.example._311_assignmentlab02;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Loan_Calculator extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label lblInterestRate = new Label("Annual Interest Rate:");
+        TextField tfInterestRate = new TextField();
+
+        Label lblYears = new Label("Number of Years:");
+        TextField tfYears = new TextField();
+
+
+
+    }
+}
